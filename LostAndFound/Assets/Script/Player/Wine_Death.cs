d6384ec2587597dd35a3520aafc64ef0e34ab242
Robert Sandh <sarobban@gmail.com>
2021-01-31 09:57:13 +0100

    wine

--- LostAndFound/Assets/Script/Player/Wine_Death.cs
+++ LostAndFound/Assets/Script/Player/Wine_Death.cs
@@ -23,8 +23,8 @@ public class Wine_Death : MonoBehaviour
 
         while (currentAngle < angleT)
         {
-            currentAngle += angleT * angleSpeed * Time.deltaTime;
-            wineOne.transform.position += Vector3.up * 2.5f * angleSpeed * Time.deltaTime;
+            currentAngle += angleT * angleSpeed * Time.deltaTime * 0.5f;
+            wineOne.transform.position += Vector3.up * 2.5f * angleSpeed * Time.deltaTime * 0.5f;
             wineOne.transform.rotation = Quaternion.Euler(0, currentAngle, 0);
             yield return null;
         }
